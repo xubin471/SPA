799503ec3c2844efd6f4bcb2fb318e9825752dbe
xubin <143147396+xubin471@users.noreply.github.com>
2026-01-02 10:59:34 +0800

    update

--- models/fewshot.py
+++ models/fewshot.py
@@ -2,26 +2,53 @@ from torch import nn
 from torch.nn import functional as F
 import torch
 from .encoder import Res50Encoder as Encoder
-# from scipy.ndimage import binary_erosion, label, distance_transform_edt
-from .loss.loss import Loss
+from scipy.ndimage import binary_erosion, label, distance_transform_edt
+from .loss import Loss
 import numpy as np
 import matplotlib.pyplot as plt
 from .SGM import SGM
-from .SAE import SAE as SAE
-# from .ca import CA
-from .GPG import GPG as GPG
+from .SAE import SAE
+from .GPG import GPG
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CA(nn.Module):
+    def __init__(self, dim=512):
+        super().__init__()
+        self.query = nn.Linear(dim, dim)
+        self.key = nn.Linear(dim, dim)
+        self.value = nn.Linear(dim, dim)
+
+        self.alpha = nn.Parameter(torch.zeros(1),requires_grad=True)
+
+    def forward(self, x1, x2):
+        q = self.query(x1)
+        k = self.key(x2)
+        v = self.value(x2)
+
+        scores = torch.matmul(q, k.transpose(-2, -1)) / (self.query.out_features ** 0.5)
+        attn = F.softmax(scores, dim=-1)
+        new_proto = torch.matmul(attn, v)
+        x1  = x1 + new_proto*self.alpha
+        return x1
+
 class FewShot(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = Encoder(replace_stride_with_dilation=[True, True, False])
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.scaler = 20
+
         self.emb_dim = 512
         self.loss = Loss()
-        self.SGM = SGM(direction_num=16)
+
+        self.SGM = SGM()
         self.criterion = nn.NLLLoss()
         self.SAE = SAE()
-        self.GPG = GPG(num_nodes=50)
+        self.CA = CA()
+        self.GPG = GPG()
 
 
     def forward(self, sup_img,qry_img, sup_msk,qry_msk,mode="train"):
@@ -33,10 +60,13 @@ class FewShot(nn.Module):
         sup_fts,qry_fts = fts[:1],fts[1:]
         sup_t, qry_t = tao[:1], tao[1:]
         self.sup_t, self.qry_t = sup_t, qry_t
+        fts_size = sup_fts.shape[-2:]
         # ================================================================================
         pred_loss = torch.zeros(1).to(self.device)
         align_loss = torch.zeros(1).to(self.device)
-        shape_loss = torch.zeros(1).to(self.device)
+        coarse_loss = torch.zeros(1).to(self.device)
+        bd_shape_loss = torch.zeros(1).to(self.device)
+        consistency_loss = torch.zeros(1).to(self.device)
         # ================================================================================
         qry_fts = self.SAE(qry_fts)
         sup_fts = self.SAE(sup_fts)
@@ -50,27 +80,37 @@ class FewShot(nn.Module):
             sup_fts =  sup_fts * shape_scores[:,:,None,None]
             qry_fts =  qry_fts * shape_scores[:,:,None,None]
         # ================================================================================
+        # sup_glob_proto, _ = self._extract_prototypes(sup_fts, sup_msk)  # [1 512]
         sup_glob_proto = self.GPG(sup_fts, sup_msk)  # [1 512]
+
         qry_sim = self.getPred(qry_fts, sup_glob_proto, self.qry_t) #[1 1 64 64]
         qry_pred = F.interpolate(qry_sim, size=img_size, mode='bilinear', align_corners=True) #[1 1 256 256]
         qry_pred_logit = torch.cat([1-qry_pred,qry_pred],dim=1) #[1 2 256 256]
-
-
+        qry_pred = torch.argmax(qry_pred_logit,dim=1) #[1 256 256]
         if mode == "train":
-            align_loss_item, shape_loss_item = self.align_shape_loss(sup_fts_bak,qry_fts_bak,sup_msk,qry_pred_logit,shape_scores)
-            align_loss += align_loss_item
-            shape_loss += shape_loss_item
-            pred_loss += self.loss.pred_loss(qry_pred_logit,qry_msk)
-            if torch.argmax(qry_pred_logit,dim=1).sum() > 1000:
-                shape_loss += 0.005 * self.loss.bd_shape_loss(qry_pred,sup_msk[None,...])
-                shape_loss += self.loss.pair_wise_consistency_loss(qry_pred_logit, qry_msk)
+            coarse_loss += self.loss.pred_loss(qry_pred_logit,qry_msk)
+        # ================================================================================
+        # qry_glob_proto, _ = self._extract_prototypes(qry_fts, qry_pred)  # [1 512]
+        qry_glob_proto = self.GPG(qry_fts, qry_pred)  # [1 512]
 
-            return qry_pred_logit, pred_loss,align_loss, shape_loss
+        final_proto = self.CA(sup_glob_proto,qry_glob_proto)
+        qry_sim = self.getPred(qry_fts, final_proto, self.qry_t) #[1 1 64 64]
+        qry_pred = F.interpolate(qry_sim, size=img_size, mode='bilinear', align_corners=True) #[1 1 256 256]
+        qry_pred_logit = torch.cat([1-qry_pred,qry_pred],dim=1) #[1 2 256 256]
+        # ================================================================================
+        if mode == "train":
+            align_loss += self.align_loss(sup_fts_bak,qry_fts_bak,sup_msk,qry_pred_logit,shape_scores)
+            pred_loss += self.loss.pred_loss(qry_pred_logit,qry_msk)
+            # if torch.argmax(qry_pred_logit,dim=1).sum() > 1000:
+            #     bd_shape_loss += 0.005 * self.loss.bd_shape_loss(qry_pred,sup_msk[None,...])
+            #     consistency_loss += self.loss.pair_wise_consistency_loss(qry_pred_logit, qry_msk)
+            # kl_shape_loss += self.loss.kl_shape_loss(qry_pred,sup_msk)
+            return qry_pred_logit, pred_loss,align_loss+coarse_loss+consistency_loss, bd_shape_loss
         else:
             return qry_pred_logit
         # ================================================================================
 
-    def align_shape_loss(self, sup_fts, qry_fts, sup_msk, qry_pred,shape_scores1):
+    def align_loss(self, sup_fts, qry_fts, sup_msk, qry_pred,shape_scores1):
         """
         Args:
             supp_fts: (1 512 64 64)
@@ -85,28 +125,38 @@ class FewShot(nn.Module):
 
         # Define loss
         align_loss = torch.zeros(1).to(self.device)
-        shape_loss = torch.zeros(1).to(self.device)
         if skip:
-            return align_loss, shape_loss
+            return align_loss
 
         # ================================================================================
         shape_scores = self.SGM(qry_fts,qry_pred_mask)
         if shape_scores1 is not None:
-            shape_loss += self.loss.l1_loss(shape_scores1,shape_scores)
+            align_loss += self.loss.l1_loss(shape_scores1,shape_scores)
         # # ================================================================================
         sup_fts = sup_fts * shape_scores[:,:,None,None]
         qry_fts = qry_fts * shape_scores[:,:,None,None]
         # ================================================================================
+        # qry_glob_proto, _ = self._extract_prototypes(qry_fts, qry_pred_mask)  # [1 512]
         qry_glob_proto = self.GPG(qry_fts, qry_pred_mask)  # [1 512]
+
         sup_sim = self.getPred(sup_fts, qry_glob_proto, self.sup_t) #[1 1 64 64]
         sup_pred = F.interpolate(sup_sim, size=sup_msk.shape[-2:], mode='bilinear', align_corners=True) #[1 1 256 256]
         sup_pred_logit = torch.cat([1-sup_pred,sup_pred],dim=1) #[1 2 256 256]
-
+        sup_pred = torch.argmax(sup_pred_logit,dim=1) #[1 256 256]
+        # =================================================================================
+        # sup_glob_proto, _ = self._extract_prototypes(sup_fts, sup_pred)  # [1 512]
+        sup_glob_proto = self.GPG(sup_fts, sup_pred)  # [1 512]
+        final_proto = self.CA(qry_glob_proto,sup_glob_proto)
+        sup_sim = self.getPred(sup_fts, final_proto, self.sup_t) #[1 1 64 64]
+        sup_pred = F.interpolate(sup_sim, size=sup_msk.shape[-2:], mode='bilinear', align_corners=True) #[1 1 256 256]
+        sup_pred_logit = torch.cat([1-sup_pred,sup_pred],dim=1) #[1 2 256 256]
+        # sup_pred = torch.argmax(sup_pred_logit,dim=1) #[1 256 256]
+        # =================================================================================
         # Compute query loss
         eps = torch.finfo(torch.float32).eps
         log_prob = torch.log(torch.clamp(sup_pred_logit, eps, 1 - eps))
         align_loss += self.criterion(log_prob, sup_msk.long())
-        return align_loss,shape_loss
+        return align_loss
 
     def getPred(self, fts, prototype, thresh):
         """
@@ -120,4 +170,15 @@ class FewShot(nn.Module):
         """
         sim = -F.cosine_similarity(fts, prototype[..., None, None], dim=1) * self.scaler
         pred = 1.0 - torch.sigmoid(0.5 * (sim - thresh))
+
         return pred[None,...] #(1 1 64 64)
+
+    def _extract_prototypes(self, sup_fts, sup_msk):
+        img_size = sup_msk.shape[-2:]
+
+        # for support set
+        sup_fts_trans = F.interpolate(sup_fts, size=img_size, mode='bilinear',
+                                      align_corners=True)  # (1 feat_dim 256 256)
+        glob_proto = torch.sum(sup_fts_trans*sup_msk[None,...],dim=(-2,-1)) / (sup_msk.sum() + 1e-8)
+
+        return glob_proto, None
